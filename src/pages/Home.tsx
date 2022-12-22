75d1b17611c844adb227240fdd2f510a64bd2052
Paul Calzado <pafcalzado@gmail.com>
2022-12-22 20:26:08 +0800

    Add tailwind

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,5 +1,18 @@
+import { Link } from 'react-router-dom';
+
 export default function Home() {
   return (
-    <div>Home</div>
-  )
+    <div className="container mx-auto flex flex-col h-full">
+      <div className="container flex flex-col items-center justify-center h-screen space-y-6">
+        <h1 className="text-6xl font-bold">budgeteer</h1>
+        <p className="text-gray-500">Know your past, control your future.</p>
+        <Link
+          className="px-4 py-2 bg-red-500 rounded-sm text-white shadow-lg uppercase font-bold hover:bg-red-600 transition-all ease duration-{250}"
+          to="/login"
+        >
+          Get Started
+        </Link>
+      </div>
+    </div>
+  );
 }
